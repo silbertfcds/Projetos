c6a7bfcd3e2e0a1bdc25093e61bc326d6e778875
silbert <silbert@focusautomacao.com.br>
2016-01-29 10:08:32 -0300

    #0005 - Erro Login

---
+++ ProFerro/src/main/resources/docs/insert.sql
@@ -0,0 +1,5 @@
+insert into cliente (doc_receita_federal, email, nome, tipoPessoa) values ('06547841236','silvio@hotmail.com','Silvio','FISICA');
+INSERT INTO usuario_grupo(usuario_id, grupo_id) VALUES(1,1);
+insert into grupo (descricao, nome) values ('ADMINISTRADOR','ADMINISTRADOR');
+insert into grupo (descricao, nome) values ('VENDEDOR','VENDEDOR');
+insert into usuario (email, nome, senha) values ('silbert.fcds@hotmail.com','Silbert','123');
